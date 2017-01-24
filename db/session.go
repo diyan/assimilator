73423ba860cf4f713667fb4d989ffd955f11c122
Alexey Diyan <alexey.diyan@gmail.com>
2017-01-24 17:34:19 +0200

    Ping DB before returning connection

--- db/session.go
+++ db/session.go
@@ -9,7 +9,13 @@ import (
 func GetSession(c echo.Context) (*dbr.Session, error) {
 	conn, err := dbr.Open("postgres", "postgres://sentry:RucLUS8A@localhost/sentry?sslmode=disable", nil)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to open db connection")
+		return nil, errors.Wrap(err, "failed to init db connection")
+	}
+	// dbr.Open calls sql.Open which returns err == nil even if there is no db connection,
+	//   so it is required to explicitly ping the database
+	err = conn.Ping()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to ping db")
 	}
 	return conn.NewSession(nil), nil
 }
