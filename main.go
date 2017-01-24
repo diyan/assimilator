2bdd03992baf972e18ec7f3050ce729a98c560fc
Alexey Diyan <alexey.diyan@gmail.com>
2017-01-24 17:33:53 +0200

    Move code from main.go to the web.GetApp func

--- main.go
+++ main.go
@@ -3,55 +3,14 @@ package main
 import (
 	"os"
 
-	"github.com/diyan/assimilator/template"
 	"github.com/diyan/assimilator/web"
-
-	"strings"
-
-	"github.com/Sirupsen/logrus"
-	"github.com/diyan/echox/log"
-	mwx "github.com/diyan/echox/middleware"
-	"github.com/echo-contrib/pongor"
-	"github.com/labstack/echo"
-	mw "github.com/labstack/echo/middleware"
-	_ "github.com/lib/pq"
-	logrusfmt "github.com/x-cray/logrus-prefixed-formatter"
 )
 
-// TODO keep main.go small, move everything to the web/server.go
-func init() {
-	template.RegisterTags()
-	template.RegisterFilters()
-}
+// TODO implement web.GetApp(), cron.GetApp(), smtp.GetApp() function
+//   see https://docs.sentry.io/server/cli/run/
 
 func main() {
-	e := echo.New()
-	e.Debug = true
-	e.Logger = log.Logrus()
-	// Register default error handler
-	e.HTTPErrorHandler = web.HTTPErrorHandler
-	e.Renderer = pongor.GetRenderer(pongor.PongorOption{Reload: true})
-	// TODO ForceColors only if codegangsta/gin detected
-	logrus.SetFormatter(&logrusfmt.TextFormatter{ShortTimestamp: true, ForceColors: true})
-	// Register access log logger
-	e.Use(mwx.LogrusLogger(nil))
-	e.Use(web.RecoverMiddleware())
-	// TODO setup route to serve static files
-	e.Static("/_static/7a9894050589851ad3c1e1a2d1adb54ac08b8832/sentry", "ui")
-	e.Pre(mw.AddTrailingSlashWithConfig(mw.TrailingSlashConfig{
-		Skipper: func(c echo.Context) bool {
-			uri := c.Request().URL.String()
-			// TODO consider check suffixes .xml .ico .txt .json .js .svg .png .gif .html .eot .woff .css .ttf .woff2
-			if strings.HasPrefix(uri, "/_static/") ||
-				strings.HasSuffix(uri, "/robots.txt") ||
-				strings.HasSuffix(uri, "/favicon.ico") ||
-				strings.HasSuffix(uri, "/crossdomain.xml") {
-				return true
-			}
-			return false
-		}}))
-
-	web.RegisterRoutes(e)
+	e := web.GetApp()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = ":3000"
---
+++ web/app.go
@@ -0,0 +1,55 @@
+package web
+
+import (
+	"strings"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/diyan/assimilator/template"
+	"github.com/diyan/echox/log"
+	"github.com/echo-contrib/pongor"
+	"github.com/labstack/echo"
+
+	mwx "github.com/diyan/echox/middleware"
+	mw "github.com/labstack/echo/middleware"
+	logrusfmt "github.com/x-cray/logrus-prefixed-formatter"
+
+	_ "github.com/lib/pq"
+)
+
+// TODO keep main.go small, move everything to the web/server.go
+func init() {
+	template.RegisterTags()
+	template.RegisterFilters()
+}
+
+func GetApp() *echo.Echo {
+	e := echo.New()
+	e.Debug = true
+	e.Logger = log.Logrus()
+	// Register default error handler
+	e.HTTPErrorHandler = HTTPErrorHandler
+	e.Renderer = pongor.GetRenderer(pongor.PongorOption{Reload: true})
+	// TODO ForceColors only if codegangsta/gin detected
+	logrus.SetFormatter(&logrusfmt.TextFormatter{ShortTimestamp: true, ForceColors: true})
+	// Register access log logger
+	e.Use(mwx.LogrusLogger(nil))
+	e.Use(RecoverMiddleware())
+	// TODO setup route to serve static files
+	e.Static("/_static/7a9894050589851ad3c1e1a2d1adb54ac08b8832/sentry", "ui")
+	e.Pre(mw.AddTrailingSlashWithConfig(mw.TrailingSlashConfig{
+		Skipper: func(c echo.Context) bool {
+			uri := c.Request().URL.String()
+			// TODO consider check suffixes .xml .ico .txt .json .js .svg .png .gif .html .eot .woff .css .ttf .woff2
+			if strings.HasPrefix(uri, "/_static/") ||
+				strings.HasSuffix(uri, "/robots.txt") ||
+				strings.HasSuffix(uri, "/favicon.ico") ||
+				strings.HasSuffix(uri, "/crossdomain.xml") {
+				return true
+			}
+			return false
+		}}))
+
+	RegisterRoutes(e)
+
+	return e
+}
