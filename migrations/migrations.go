fa9f569b6d9cc6451aae9f6b59eb926d312d04f6
Alexey Diyan <alexey.diyan@gmail.com>
2017-02-12 18:33:32 +0200

    Use relative path for db migrations

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -3,6 +3,8 @@ package migrations
 import (
 
 	// Enable PostgreSQL driver for migration tool
+	"path/filepath"
+
 	_ "github.com/mattes/migrate/driver/postgres"
 	"github.com/mattes/migrate/migrate"
 	"github.com/pkg/errors"
@@ -11,7 +13,12 @@ import (
 func UpgradeDB(databaseURL string) error {
 	// TODO Who is responsible to run `create database sentry_ci` statement?
 	// TODO consider use async version and optimize app's startup time
-	allErrors, ok := migrate.UpSync(databaseURL, "/home/alexey/go/src/github.com/diyan/assimilator/migrations/postgres")
+	// TODO this relative path works only for single test package, use go.rice instead
+	migrationsPath, err := filepath.Abs("../../migrations/postgres")
+	if err != nil {
+		return errors.Wrap(err, "can not get absolute path for db migrations")
+	}
+	allErrors, ok := migrate.UpSync(databaseURL, migrationsPath)
 	if !ok {
 		// TODO handle error, do not just return the first one
 		// pp.Print(allErrors)
