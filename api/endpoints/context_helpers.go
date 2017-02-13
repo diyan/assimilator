bdad1a9d5e902c11f0854bbc61a1e4e8136c9bba
Alexey Diyan <alexey.diyan@gmail.com>
2017-02-13 17:25:14 +0200

    Use safe type assertion in GetProjectID

--- api/endpoints/context_helpers.go
+++ api/endpoints/context_helpers.go
@@ -1,8 +1,15 @@
 package api
 
-import "github.com/labstack/echo"
+import (
+	"errors"
+
+	"github.com/labstack/echo"
+)
 
 // GetProjectID returns projectID for current HTTP request
 func GetProjectID(c echo.Context) int64 {
-	return c.Get("projectID").(int64)
+	if projectID, ok := c.Get("projectID").(int64); ok {
+		return projectID
+	}
+	panic(errors.New("failed to get projectID, is mw.RequireProject call missing?"))
 }
