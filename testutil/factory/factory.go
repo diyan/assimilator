00e1223cc0a7a15cd6fdfa77d5c9d2a827a0cb12
Alexey Diyan <alexey.diyan@gmail.com>
2017-02-13 15:39:26 +0200

    Refactor TestFactory
    - Reduce complexity of New func
    - Remove closures, use regular methods
    - Extract code into MockDB func

--- testutil/factory/factory.go
+++ testutil/factory/factory.go
@@ -13,48 +13,57 @@ import (
 )
 
 type TestFactory struct {
-	t                *testing.T
-	tx               *dbr.Tx
-	Reset            func()
-	SaveOrganization func(org models.Organization)
-	SaveProject      func(project models.Project)
-	SaveTags         func(tags ...*models.TagKey)
+	t   *testing.T
+	tx  *dbr.Tx
+	ctx echo.Context
 }
 
 func New(t *testing.T, server *echo.Echo) TestFactory {
 	noError := require.New(t).NoError
-	ctx := server.NewContext(nil, nil)
-	// TODO remove hack
-	ctx.Set("conf.Config", MakeAppConfig())
-	tx, err := db.GetTx(ctx)
+	tx, err := db.New(MakeAppConfig())
 	noError(err)
+	MockDB(server, tx)
+	ctx := server.NewContext(nil, nil)
+	db.ToE(ctx, tx)
 	tf := TestFactory{
-		t:  t,
-		tx: tx,
+		t:   t,
+		tx:  tx,
+		ctx: ctx,
 	}
-	tf.Reset = func() {
-		err := tf.tx.Rollback()
-		noError(err)
-	}
+	return tf
+}
 
-	// TODO Tricky implementation. Mock *dbr.Tx in the test Echo instance
+func (tf TestFactory) noError(err error, msgAndArgs ...interface{}) {
+	require.New(tf.t).NoError(err, msgAndArgs)
+}
+
+func (tf TestFactory) Reset() {
+	err := tf.tx.Rollback()
+	tf.noError(err)
+}
+
+func (tf TestFactory) SaveOrganization(org models.Organization) {
+	orgStore := store.NewOrganizationStore(tf.ctx)
+	tf.noError(orgStore.SaveOrganization(org))
+}
+
+func (tf TestFactory) SaveProject(project models.Project) {
+	projectStore := store.NewProjectStore(tf.ctx)
+	tf.noError(projectStore.SaveProject(project))
+}
+
+func (tf TestFactory) SaveTags(tags ...*models.TagKey) {
+	projectStore := store.NewProjectStore(tf.ctx)
+	tf.noError(projectStore.SaveTags(tags...))
+}
+
+// MockDB adds early middleware that mock DB transaction to the test Echo instance
+// TODO consider move this to the db or db_test package
+func MockDB(server *echo.Echo, tx *dbr.Tx) {
 	server.Pre(func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			c.Set("dbr.Tx", tx)
+			db.ToE(c, tx)
 			return next(c)
 		}
 	})
-
-	orgStore := store.NewOrganizationStore(ctx)
-	projectStore := store.NewProjectStore(ctx)
-	tf.SaveOrganization = func(org models.Organization) {
-		noError(orgStore.SaveOrganization(org))
-	}
-	tf.SaveProject = func(project models.Project) {
-		noError(projectStore.SaveProject(project))
-	}
-	tf.SaveTags = func(tags ...*models.TagKey) {
-		noError(projectStore.SaveTags(tags...))
-	}
-	return tf
 }
