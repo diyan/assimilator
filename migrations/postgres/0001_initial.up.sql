9e61611fc6bb57f4758802ba82c77d1bddcbdf4f
Alexey Diyan <alexey.diyan@gmail.com>
2017-02-13 19:17:22 +0200

    Add more tables into init migration, use serial PK

--- migrations/postgres/0001_initial.up.sql
+++ migrations/postgres/0001_initial.up.sql
@@ -1,8 +1,5 @@
--- todo consider bigserial, serial instead of sequence
---   see http://stackoverflow.com/questions/20781111/postgresql-9-1-primary-key-autoincrement
-
 create table sentry_project (
-    id integer not null,
+    id serial not null,
     name character varying(200) not null,
     public boolean not null,
     date_added timestamp with time zone not null,
@@ -15,18 +12,10 @@ create table sentry_project (
     constraint ck_status_pstv_3af8360b8a37db73 check ((status >= 0)),
     constraint sentry_project_status_check check ((status >= 0))
 );
-create sequence sentry_project_id_seq
-    start with 1
-    increment by 1
-    no minvalue
-    no maxvalue
-    cache 1;
-alter sequence sentry_project_id_seq owned by sentry_project.id;
-
 
 
 create table sentry_filterkey (
-    id integer not null,
+    id serial not null,
     project_id integer not null,
     key varchar(32) not null,
     values_seen integer not null,
@@ -37,18 +26,10 @@ create table sentry_filterkey (
     constraint sentry_filterkey_status_check check ((status >= 0)),
     constraint sentry_filterkey_values_seen_check check ((values_seen >= 0))
 );
-create sequence sentry_filterkey_id_seq
-    start with 1
-    increment by 1
-    no minvalue
-    no maxvalue
-    cache 1;
-alter sequence sentry_filterkey_id_seq owned by sentry_filterkey.id;
-
 
 
 create table sentry_organization (
-    id integer not null,
+    id serial not null,
     name character varying(64) not null,
     status integer not null,
     date_added timestamp with time zone not null,
@@ -57,10 +38,53 @@ create table sentry_organization (
     default_role character varying(32) not null,
     constraint sentry_organization_status_check check ((status >= 0))
 );
-create sequence sentry_organization_id_seq
-    start with 1
-    increment by 1
-    no minvalue
-    no maxvalue
-    cache 1;
-alter sequence sentry_organization_id_seq owned by sentry_organization.id;
+
+
+create table sentry_organizationmember (
+    id serial not null,
+    organization_id integer not null,
+    user_id integer,
+    type integer not null,
+    date_added timestamp with time zone not null,
+    email character varying(75),
+    has_global_access boolean not null,
+    flags bigint not null,
+    role character varying(32) not null,
+    token character varying(64),
+    constraint sentry_organizationmember_type_check check ((type >= 0))
+);
+
+
+create table sentry_organizationoptions (
+    id serial not null,
+    organization_id integer not null,
+    key character varying(64) not null,
+    value text not null
+);
+
+
+create table sentry_team (
+    id serial not null,
+    slug character varying(50) not null,
+    name character varying(64) not null,
+    date_added timestamp with time zone,
+    status integer not null,
+    organization_id integer not null,
+    constraint ck_status_pstv_1772e42d30eba7ba check ((status >= 0)),
+    constraint sentry_team_status_check check ((status >= 0))
+);
+
+
+create table sentry_organizationmember_teams (
+    id serial not null,
+    organizationmember_id integer not null,
+    team_id integer not null,
+    is_active boolean not null
+);
+
+
+create table sentry_organizationaccessrequest (
+    id serial not null,
+    team_id integer not null,
+    member_id integer not null
+);
