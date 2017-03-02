8a55ab207daeb27609a2f4de402fdfde787d87e1
Alexey Diyan <alexey.diyan@gmail.com>
2017-03-02 20:56:12 +0200

    Add workaround for store event route

--- web/api/routes.go
+++ web/api/routes.go
@@ -9,9 +9,11 @@ func RegisterAPIRoutes(g *echo.Group) {
 	// Store endpoints first since they are the most active
 	//e.GET("/api/store/", storeGetView)
 	//e.POST("/api/store/", storePostView)
-	g = g.Group("/store")
-	g.GET("/", storeGetView)
-	g.POST("/", storePostView)
+
+	// TODO Can not register same handler for two different routes
+	//g = g.Group("/store")
+	//g.GET("/", storeGetView)
+	//g.POST("/", storePostView)
 	// :project_id is [\w_-]+
 	g = g.Group("/:project_id/store")
 	g.GET("/", storeGetView)
