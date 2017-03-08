636bc890e9100245227240939e86a1bf79166f9f
Alexey Diyan <alexey.diyan@gmail.com>
2017-03-08 19:59:34 +0200

    Stub HTTP interface

--- interfaces/http.go
+++ interfaces/http.go
@@ -4,3 +4,11 @@ type HTTP struct {
 	Headers map[string]string `json:"headers"`
 	URL     string            `json:"url"`
 }
+
+func (request *HTTP) UnmarshalRecord(nodeBlob interface{}) error {
+	return nil
+}
+
+func (request *HTTP) UnmarshalAPI(rawEvent map[string]interface{}) error {
+	return nil
+}
