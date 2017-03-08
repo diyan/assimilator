df16ad519fc922f3505f330c35725399551a020d
Alexey Diyan <alexey.diyan@gmail.com>
2017-03-08 18:11:07 +0200

    Implement User.UnmarshalAPI

--- interfaces/user.go
+++ interfaces/user.go
@@ -1,7 +1,50 @@
 package interfaces
 
+import "fmt"
+
+// User is an interface which describes the authenticated User for a request.
+//
+// You should provide **at least** either an `id` (a unique identifier for
+// an authenticated user) or `ip_address` (their IP address).
+//
+// All other attributes are optional.
+//
+// {
+//     "id": "unique_id",
+//     "username": "my_user",
+//     "email": "foo@example.com"
+//     "ip_address": "127.0.0.1",
+//     "optional": "value"
+// }
 type User struct {
-	ID        int    `json:"id"`
+	ID        string `json:"id"`
+	Username  string `json:"username"`
 	Email     string `json:"email"`
 	IPAddress string `json:"ip_address"`
+	// Is Sentry allows arbitrary key/value pairs for User interface?
+	Extra map[string]string `json:"-"`
+}
+
+func (user *User) UnmarshalRecord(nodeBlob interface{}) error {
+	return nil
+}
+
+func (user *User) UnmarshalAPI(rawEvent map[string]interface{}) error {
+	if rawUser, ok := rawEvent["user"].(map[string]interface{}); ok {
+		// TODO validate input
+		user.ID = anyTypeToString(rawUser["id"])
+		user.Username = anyTypeToString(rawUser["username"])
+		user.Email = anyTypeToString(rawUser["email"])
+		user.IPAddress = anyTypeToString(rawUser["ip_address"])
+		// TODO process extra fields
+	}
+	return nil
+}
+
+// TODO duplicated code in interfaces and web/api/store_event.go
+func anyTypeToString(v interface{}) string {
+	if v != nil {
+		return fmt.Sprint(v)
+	}
+	return ""
 }
