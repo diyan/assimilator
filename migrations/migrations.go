3f90026f236689c714f7f601f8d50fd186695829
Alexey Diyan <alexey.diyan@gmail.com>
2017-03-14 21:09:15 +0200

    Remove obsolete code from migration package

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -1,19 +1,17 @@
 package migrations
 
 import (
+	"github.com/diyan/assimilator/migrations/source"
+
 	"github.com/GeertJohan/go.rice"
 	"github.com/mattes/migrate"
 	"github.com/pkg/errors"
 	// Enable PostgreSQL driver for migration tool
 	_ "github.com/mattes/migrate/database/postgres"
-	// Enable File source for migration tool
-	_ "github.com/mattes/migrate/source/file"
-	// Enable go.rice source for migration tool
-	"github.com/diyan/assimilator/migrations/source"
 )
 
 func UpgradeDB(databaseURL string) error {
-	// TODO Who is responsible to run `create database sentry_ci` statement?
+	// TODO Who should run `create database sentry_ci` statement?
 	box, err := rice.FindBox("postgres")
 	if err != nil {
 		return errors.Wrap(err, "can not find db migrations")
