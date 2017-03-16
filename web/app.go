355667d99a0bf5b02e231930448484cf0375caf9
Alexey Diyan <alexey.diyan@gmail.com>
2017-03-16 13:39:32 +0200

    Serve UI from rice box

--- web/app.go
+++ web/app.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"net/http"
 	"strings"
 
 	"github.com/diyan/assimilator/conf"
@@ -27,16 +28,23 @@ func NewApp(config conf.Config) *echo.Echo {
 	e.Use(conf.NewMiddleware(config))
 	e.Use(log.NewAccessLogMiddleware(config))
 	e.Use(recover.NewMiddleware(config))
-	// TODO setup route to serve static files
-	e.Static("/_static/7a9894050589851ad3c1e1a2d1adb54ac08b8832/sentry", "ui")
+	uiBox, err := rice.FindBox("../ui")
+	if err != nil {
+		panic(errors.Wrap(err, "can not find ui box"))
+	}
+	// TODO Extract into ui.RegisterRoutes(e) func
+	uiPrefix := "/_static/7a9894050589851ad3c1e1a2d1adb54ac08b8832/sentry/"
+	e.GET(
+		uiPrefix+"*",
+		echo.WrapHandler(http.StripPrefix(uiPrefix, http.FileServer(uiBox.HTTPBox()))))
 	e.Pre(mw.AddTrailingSlashWithConfig(mw.TrailingSlashConfig{
 		Skipper: func(c echo.Context) bool {
-			uri := c.Request().URL.String()
+			path := c.Request().URL.Path
 			// TODO consider check suffixes .xml .ico .txt .json .js .svg .png .gif .html .eot .woff .css .ttf .woff2
-			if strings.HasPrefix(uri, "/_static/") ||
-				strings.HasSuffix(uri, "/robots.txt") ||
-				strings.HasSuffix(uri, "/favicon.ico") ||
-				strings.HasSuffix(uri, "/crossdomain.xml") {
+			if strings.HasPrefix(path, "/_static/") ||
+				strings.HasSuffix(path, "/robots.txt") ||
+				strings.HasSuffix(path, "/favicon.ico") ||
+				strings.HasSuffix(path, "/crossdomain.xml") {
 				return true
 			}
 			return false
