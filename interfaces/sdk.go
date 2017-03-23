d4ad838a57b3dbb2d4bd48c565325b98472bd6b7
Alexey Diyan <alexey.diyan@gmail.com>
2017-03-23 20:26:59 +0200

    Use mapstructure for interfaces.SDK

--- interfaces/sdk.go
+++ interfaces/sdk.go
@@ -1,9 +1,9 @@
 package interfaces
 
 type SDK struct {
-	Name     string   `json:"name" node:"name"`
-	Version  string   `json:"version" node:"version"`
-	ClientIP string   `json:"clientIP" node:"client_ip"`
+	Name     string   `node:"name"      json:"name"`
+	Version  string   `node:"version"   json:"version"`
+	ClientIP string   `node:"client_ip" json:"clientIP"`
 	Upstream Upstream `json:"upstream"`
 }
 
@@ -13,13 +13,13 @@ type Upstream struct {
 	IsNewer bool   `json:"isNewer"`
 }
 
-func (sdk *SDK) UnmarshalRecord(nodeBlob interface{}) error {
-	err := DecodeRecord("sdk", "sentry.interfaces.Sdk", nodeBlob, sdk)
+func (sdk *SDK) DecodeRecord(record interface{}) error {
+	err := DecodeRecord("sdk", "sentry.interfaces.Sdk", record, sdk)
 	sdk.Upstream.Name = sdk.Name                                // TODO check original code
 	sdk.Upstream.URL = "https://docs.sentry.io/clients/python/" // TODO remove hardcode
 	return err
 }
 
-func (sdk *SDK) UnmarshalAPI(rawEvent map[string]interface{}) error {
+func (sdk *SDK) DecodeRequest(request map[string]interface{}) error {
 	return nil
 }
