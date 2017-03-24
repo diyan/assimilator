6c43d4c27c688a0c84241898cec4e5bb3781f999
Alexey Diyan <alexey.diyan@gmail.com>
2017-03-24 15:40:41 +0200

    Implement Repos.DecodeRequest

--- interfaces/repos.go
+++ interfaces/repos.go
@@ -1,6 +1,23 @@
 package interfaces
 
-type Repos struct {
+// Repos contains details about repositories connected to an event.
+// This is primarily used to aid with mapping the application code's filepath
+// to the equivilent path inside of a repository.
+//
+// {
+//     "/abs/path/to/sentry": {
+//         "name": "getsentry/sentry",
+//         "prefix": "src",
+//         "revision": "..." // optional
+//     }
+// }
+type Repos map[string]Repo
+
+// Repo contains details about one repository for the Repos interface.
+type Repo struct {
+	Name     string  `kv:"name"     in:"name"     json:"name"`
+	Prefix   string  `kv:"prefix"   in:"prefix"   json:"prefix"`
+	Revision *string `kv:"revision" in:"revision" json:"revision"`
 }
 
 func (repos *Repos) DecodeRecord(record interface{}) error {
