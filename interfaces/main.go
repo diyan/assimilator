2eee5526f2964681e7d140cece7cec00ffcf11a0
Alexey Diyan <alexey.diyan@gmail.com>
2017-03-27 15:45:03 +0300

    Update EventInterfaces
    - Add errNoValue error
    - Populate entries array for JSON response
    - Use nil value for interfaces that was not provided

--- interfaces/main.go
+++ interfaces/main.go
@@ -7,89 +7,135 @@ import (
 	"github.com/pkg/errors"
 )
 
-type EventInterfaces struct {
-	// Built-in interfaces, sorted by name
-	AppleCrashReport models.Marshaler
-	Breadcrumbs      models.Marshaler
-	Contexts         models.Marshaler
-	CSP              models.Marshaler
-	DebugMeta        models.Marshaler
-	Device           models.Marshaler
-	Exception        models.Marshaler
-	LogEntry         models.Marshaler
-	Query            models.Marshaler
-	Repos            models.Marshaler
-	Request          models.Marshaler
-	SDK              models.Marshaler
-	Stacktrace       models.Marshaler
-	Template         models.Marshaler
-	Threads          models.Marshaler
-	User             models.Marshaler
+var errNoValue = errors.New("value was not provided")
+
+type EventEntry struct {
+	Type  string           `json:"type"`
+	Value models.Marshaler `json:"data"`
 }
 
-func DecodeRecord(keyAlias, keyCanonical string, record interface{}, target interface{}) error {
+type EventInterfaces struct {
+	// Built-in interfaces, sorted by name
+	AppleCrashReport models.Marshaler `json:"-"`
+	Breadcrumbs      models.Marshaler `json:"-"`
+	Contexts         models.Marshaler `json:"contexts,omitempty"`
+	CSP              models.Marshaler `json:"-"`
+	DebugMeta        models.Marshaler `json:"-"`
+	Device           models.Marshaler `json:"device,omitempty"`
+	Exception        models.Marshaler `json:"-"`
+	LogEntry         models.Marshaler `json:"-"`
+	Query            models.Marshaler `json:"-"`
+	Repos            models.Marshaler `json:"-"`
+	Request          models.Marshaler `json:"-"`
+	SDK              models.Marshaler `json:"sdk,omitempty"`
+	Stacktrace       models.Marshaler `json:"-"`
+	Template         models.Marshaler `json:"-"`
+	Threads          models.Marshaler `json:"-"`
+	User             models.Marshaler `json:"user"` // TODO omitempty?
+
+	Message string       `json:"message"`
+	Entries []EventEntry `json:"entries"` // TODO omitempty?
+}
+
+func DecodeRecord(record interface{}, target models.Marshaler) error {
 	recordMap, ok := record.(map[interface{}]interface{})
 	if !ok {
 		return nil
 	}
-	value, ok := recordMap[keyAlias]
+	value, ok := recordMap[target.KeyAlias()]
 	if !ok {
-		value, ok = recordMap[keyCanonical]
+		value, ok = recordMap[target.KeyCanonical()]
 		if !ok {
-			return nil
+			return errNoValue
 		}
 	}
 	err := models.DecodeRecord(value, target)
-	return errors.Wrapf(err, fmt.Sprintf("can not decode node record to %s", keyCanonical))
+	return errors.Wrapf(err, fmt.Sprintf("can not decode node record to %s", target.KeyCanonical()))
 }
 
-func DecodeRequest(keyAlias, keyCanonical string, request map[string]interface{}, target interface{}) error {
-	value, ok := request[keyAlias].(map[string]interface{})
+func DecodeRequest(request map[string]interface{}, target models.Marshaler) error {
+	value, ok := request[target.KeyAlias()].(map[string]interface{})
 	if !ok {
-		value, ok = request[keyCanonical].(map[string]interface{})
+		value, ok = request[target.KeyCanonical()].(map[string]interface{})
 		if !ok {
 			return nil
 		}
 	}
 	err := models.DecodeRequest(value, target)
-	return errors.Wrapf(err, fmt.Sprintf("can not decode request to %s", keyCanonical))
+	return errors.Wrapf(err, fmt.Sprintf("can not decode request to %s", target.KeyCanonical()))
 }
 
 func (event *EventInterfaces) DecodeRecord(record interface{}) error {
 	// TODO too many known types here, use interfaces instead
 	// TODO add error handling
-	event.AppleCrashReport = &AppleCrashReport{}
-	event.AppleCrashReport.DecodeRecord(record)
-	event.Breadcrumbs = &Breadcrumbs{}
-	event.Breadcrumbs.DecodeRecord(record)
+	var value models.Marshaler = &AppleCrashReport{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.AppleCrashReport = value
+	}
+	value = &Breadcrumbs{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.Breadcrumbs = value
+	}
+	value = &Contexts{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.Contexts = value
+	}
+	// TODO remove hardcode
 	event.Contexts = &Contexts{}
-	event.Contexts.DecodeRecord(record)
-	event.CSP = &CSP{}
-	event.CSP.DecodeRecord(record)
-	event.DebugMeta = &DebugMeta{}
-	event.DebugMeta.DecodeRecord(record)
-	event.Device = &Device{}
-	event.Device.DecodeRecord(record)
-	event.Exception = &Exception{}
-	event.Exception.DecodeRecord(record)
-	event.Request = &HTTP{}
-	event.Request.DecodeRecord(record)
-	event.LogEntry = &LogEntry{}
-	event.LogEntry.DecodeRecord(record)
-	event.Query = &Query{}
-	event.Query.DecodeRecord(record)
-	event.Repos = &Repos{}
-	event.Repos.DecodeRecord(record)
-	event.SDK = &SDK{}
-	event.SDK.DecodeRecord(record)
-	event.Stacktrace = &Stacktrace{}
-	event.Stacktrace.DecodeRecord(record)
-	event.Template = &Template{}
-	event.Template.DecodeRecord(record)
-	event.Threads = &Threads{}
-	event.Threads.DecodeRecord(record)
-	event.User = &User{}
-	event.User.DecodeRecord(record)
+	value = &CSP{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.CSP = value
+	}
+	value = &DebugMeta{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.DebugMeta = value
+	}
+	value = &Device{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.Device = value
+	}
+	value = &Exception{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.Exception = value
+	}
+	value = &HTTP{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.Request = value
+	}
+	value = &LogEntry{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.LogEntry = value
+	}
+	value = &Query{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.Query = value
+	}
+	value = &Repos{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.Repos = value
+	}
+	value = &SDK{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.SDK = value
+	}
+	value = &Stacktrace{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.Stacktrace = value
+	}
+	value = &Template{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.Template = value
+	}
+	value = &Threads{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.Threads = value
+	}
+	value = &User{}
+	if err := value.DecodeRecord(record); err == nil {
+		event.User = value
+	}
+
+	fillEntries(event)
 	return nil
 }
 
@@ -129,4 +175,38 @@ func (event *EventInterfaces) DecodeRequest(request map[string]interface{}) erro
 	event.User = &User{}
 	event.User.DecodeRequest(request)
 	return nil
+}
+
+func fillEntries(event *EventInterfaces) {
+	// TODO remove hardcode
+	event.Message = event.LogEntry.(*LogEntry).Message
+	// Reserved interfaces which remains in the root: User, SDK, Device, Contexts
+	interfaces := []models.Marshaler{
+		event.AppleCrashReport,
+		event.Breadcrumbs,
+		event.CSP,
+		event.DebugMeta,
+		event.Exception,
+		event.LogEntry,
+		event.Query,
+		event.Repos,
+		event.Request,
+		event.Stacktrace,
+		event.Template,
+		event.Threads,
+	}
+	for _, value := range interfaces {
+		if value == nil {
+			continue
+		}
+		// TODO remove hardcode
+		typeName := value.KeyAlias()
+		if typeName == "logentry" {
+			typeName = "message"
+		}
+		event.Entries = append(event.Entries, EventEntry{
+			Type:  typeName,
+			Value: value,
+		})
+	}
 }
